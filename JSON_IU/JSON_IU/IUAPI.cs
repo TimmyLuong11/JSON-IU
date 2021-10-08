04fe140782baffe5340086cbf02d9b473f606a5d
TimmyLuong <timmyluong11@gmail.com>
2021-10-08 02:12:52 -0500

    Working photo not gif

---
+++ JSON_IU/JSON_IU/IUAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_IU
+{
+    class IUAPI
+    {
+        public string love { get; set; }
+        public string document { get; set; }
+    }
+}
